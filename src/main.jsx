4971ab6194b7511c0b107b02e13060e9242b8ec7
khushnam <chauhankhushnam@gmail.com>
2024-04-15 19:47:25 +0530

    device responsive

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
